62da8885e36eddf6cd4f148bcd9934c315963f53
roy2pushkar <roy2000pushkar@gmail.com>
2023-07-08 20:37:24 +0530

    Hackweb3conf

--- src/components/NavbarItems.jsx
+++ src/components/NavbarItems.jsx
@@ -1,5 +1,7 @@
 
 import React from "react";
+import dark from '../images/dark-mode.png';
+import day from '../images/day-mode.png';
 
 function NavbarItems() {
   return (
@@ -11,7 +13,8 @@ function NavbarItems() {
               StudyNotion
             </a>
           </div>
-          <div className="flex gap-4 mx-6 justify-between items-center">
+        
+            <div className="flex gap-4 mx-6 justify-between items-center">
             <a href="#home" className="hover:text-gray-600 hover:font-semibold">
               Home
             </a>
@@ -27,10 +30,16 @@ function NavbarItems() {
             >
               Contact
             </a>
+            <div>
+          <img src= {dark}  className=" cursor-pointer " alt="day-img" /> 
           </div>
+          </div>
+        
+         
         </div>
-        <div className="md:hidden">
-          <button className="flex items-center px-3 py-2 border rounded text-white border-white hover:text-gray-400 hover:border-gray-400">
+        <div className="" >
+          <div className="md:hidden ">
+            <button className="flex items-center px-3 py-2 border rounded text-white border-white hover:text-gray-400 hover:border-gray-400">
             <svg
               className="fill-current h-3 w-3"
               viewBox="0 0 20 20"
@@ -40,6 +49,13 @@ function NavbarItems() {
               <path d="M0 3h20v2H0zm0 6h20v2H0zm0 6h20v2H0z" />
             </svg>
           </button>
+          </div>
+         
+          <div className="md:hidden">
+        
+          <img src= {day}  className="" alt="day-img" /> 
+          
+          </div>
         </div>
       </nav>
     </div>
