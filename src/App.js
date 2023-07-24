555736028eb1271dcf06a2dd2ff5670f3b0fb7ac
roy2pushkar <roy2000pushkar@gmail.com>
2023-07-24 18:50:33 +0530

    HackWeb3Conf

--- src/App.js
+++ src/App.js
@@ -26,6 +26,7 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" element={<Front />} />
+        <Route path="/mainpage" element={<MainPage />} />
 
         <Route path="/crypto" element={<Crypto />} />
         <Route path="/blockchain" element={<BlockChain />} />
